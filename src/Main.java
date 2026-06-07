8626fffd21d2686b1b25449c5e600f20b3bd7dfd
Пользователь <777draft777@mail.ru>
2026-06-07 19:07:48 +0300

    Задание 1

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,13 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-
-        }
+        System.out.println("Задание 1");
+        int client0s = 1;
+        System.out.println("Операционная система 0 — iOS, 1 — Android");
+        if (client0s==0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        }else if (client0s==1){
+            System.out.println("Установите версию приложения для Android по ссылке");
     }
+}
 }
